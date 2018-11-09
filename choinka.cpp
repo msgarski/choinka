806292636dc9c89576877a1a740d1f690369e4c6
Robert Gorski <msgaarski@gmail.com>
2018-11-09 06:56:31 +0100

    pierwsza proba wysylki

---
+++ choinka.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int h=10;
+	int p, s, g;
+	for(p=1; p<=h; p++)
+	{
+		for(s=1; s<=h-p; s++)
+		{
+			std::cout<<" ";
+		}
+		std::cout<<p;
+		for(g=1; g<=p*2-1; g++)
+		{
+			std::cout<<"*";
+		}
+		std::cout<<p<<std::endl;
+	}
+	
+	
+			
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
